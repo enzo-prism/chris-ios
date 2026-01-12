a180824d8dcf0f92833e1dc0de97219cbc676259
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2026-01-11 19:12:30 -0800

    Initial Commit

---
+++ Chris/ChrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChrisApp.swift
+//  Chris
+//
+//  Created by Enzo on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ChrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
